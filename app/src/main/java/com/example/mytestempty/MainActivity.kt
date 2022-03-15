3a80c3b82686d90704821e6456ba2d60b317bd85
Jakub <drwal.j04@htlwienwest.at>
2022-03-15 12:31:31 +0100

    commit 1

---
+++ app/src/main/java/com/example/mytestempty/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.mytestempty
+
+import android.content.ClipData
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.net.Socket
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val lvlButton = findViewById<Button>(R.id.button)
+        val pointButton = findViewById<Button>(R.id.pointsAdden)
+
+        val daTextView = findViewById<TextView>(R.id.textView)
+        val ptAnzeige = findViewById<TextView>(R.id.pointAnzeige)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val dalvlView = findViewById<TextView>(R.id.lvlAnzeige)
+        val infoSender = findViewById<Button>(R.id.infoSender)
+
+        var level = 1
+        var points = 0
+        var progress = 0
+/*
+        fun sendstring(info:String) {
+            val s1 = DatagramSocket(6789, InetAddress.getLocalHost() )
+            val client = Socket(InetAddress.getLocalHost(), 6789)
+            var d1 = DatagramPacket()
+            s1.send()
+            client.outputStream.write(info.length)
+            client.outputStream.write(info.toByteArray())
+            client.close()
+        }
+
+        fun postItem(item: ClipData.Item) {
+            val token = sendstring("t")
+            val post = submitPost(token, item)
+            processPost(post)
+        }
+
+
+
+
+        infoSender.setOnClickListener{
+            send("fortnite")
+        }*/
+
+        pointButton.setOnClickListener{
+            points++
+            ptAnzeige.text= "Points: $points"
+        }
+
+        lvlButton.setOnClickListener{
+            if (points >=10){
+                if (progress + 10 >= 30){
+                    level ++
+                    dalvlView.text = "lvl: $level"
+                    points -=10
+                    ptAnzeige.text= "Points: $points"
+                    Toast.makeText(this@MainActivity, "Leveled Up!", Toast.LENGTH_SHORT).show()
+                    progress = 0
+                    progressBar.progress = progress
+
+                }
+                else {
+                    points -=10
+                    ptAnzeige.text= "Points: $points"
+                    progress +=10
+                    progressBar.progress = (progress*3)
+                }
+            }
+
+        }
+
+    }
+}
